b15398e7a8f6eaf4e8aafe32838def5000084b17
chudanqin <hzzhudanqin@corp.netease.com>
2019-01-23 14:28:48 +0800

    Initial Commit

---
+++ Monkey/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  Monkey
+//
+//  Created by chudanqin on 2019/1/23.
+//  Copyright © 2019 chudanqin. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ Monkey/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  Monkey
+//
+//  Created by chudanqin on 2019/1/23.
+//  Copyright © 2019 chudanqin. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
